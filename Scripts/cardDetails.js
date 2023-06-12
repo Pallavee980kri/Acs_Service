ed65a0b8d19f3c7b993ebef66849a787b22d5e64
Pallavee980kri <pallavee.kumari@razorpay.com>
2023-06-12 23:38:15 +0530

    working_on_ui

---
+++ Scripts/cardDetails.js
@@ -0,0 +1,32 @@
+
+//creating options for Expiry Year 
+
+function AddExpiryYearOption(){
+let expiryyear=document.getElementById("expiryYear")
+let arr=new Array(100).fill(1)
+arr.map((element,index)=>{
+    let option=document.createElement("option")
+    option.value=2023+index;
+    option.textContent=2023+index;
+    expiryyear.append(option)
+})
+}
+AddExpiryYearOption()
+
+
+function handleSubmit(){
+event.preventDefault()
+
+let form=document.getElementById("form")
+let cardNumber=form.cardNumber.value;
+let cardHolderName=form.cardHolderName.value;
+let cvv=form.cvv.value;
+let expiryMonth=form.expiryMonth.value;
+let expiryYear=form.expiryYear.value;
+let formContent={
+    cardNumber,cardHolderName,cvv,expiryMonth,expiryYear
+}
+console.log(formContent)
+}
+
+
