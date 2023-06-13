b6df893d302741627676de8a80560cd457a5bd47
Pallavee980kri <pallavee.kumari@razorpay.com>
2023-06-14 00:34:03 +0530

    working_on_validation_features

--- Frontend/Scripts/cardDetails.js
+++ Frontend/Scripts/cardDetails.js
@@ -1,6 +1,13 @@
 //creating options for Expiry Year
+var payNowButtonFlag = false;
+var cardNumberValidFlag = true;
 
-function AddExpiryYearOption() {
+let submitButton = document.getElementById("submitButton");
+let cardNumberErrorMessage = document.getElementById("cardNumberErrorMessage");
+cardNumberErrorMessage.style.display = "none";
+submitButton.disabled = true;
+
+const AddExpiryYearOption = () => {
   let expiryyear = document.getElementById("expiryYear");
   for (let index = 0; index < 99; index++) {
     let option = document.createElement("option");
@@ -8,10 +15,10 @@ function AddExpiryYearOption() {
     option.textContent = 2023 + index;
     expiryyear.append(option);
   }
-}
+};
 AddExpiryYearOption();
 
-function handleSubmit() {
+const handleSubmit = () => {
   event.preventDefault();
 
   let form = document.getElementById("form");
@@ -27,39 +34,100 @@ function handleSubmit() {
     expiryMonth,
     expiryYear,
   };
-}
+  console.log(formContent);
+};
 //validation for card number
-function handleValidationForCardNumber() {
+const handleValidationForCardNumber = () => {
   let form = document.getElementById("form");
   let cardNumber = form.cardNumber.value;
 
-  if (event.keyCode == 69 || event.keyCode == 187 || event.keyCode == 190) {
+  if (
+    event.keyCode == 69 ||
+    event.keyCode == 187 ||
+    event.keyCode == 190 ||
+    event.keyCode == 189
+  ) {
     event.preventDefault();
   }
 
-  if (event.keyCode !== 8) {
+  if (
+    event.keyCode !== 8 &&
+    event.keyCode !== 37 &&
+    event.keyCode !== 39 &&
+    event.keyCode !== 46
+  ) {
     if (cardNumber.length === 16) {
       event.preventDefault();
     }
   }
-}
+  if (cardNumber.length == 16) {
+    let cardNumberErrorMessage = document.getElementById(
+      "cardNumberErrorMessage"
+    );
+    cardNumberErrorMessage.style.display = "none";
+    cardNumberValidFlag = true;
+  } else {
+    cardNumberValidFlag = false;
+    let cardNumberErrorMessage = document.getElementById(
+      "cardNumberErrorMessage"
+    );
+    cardNumberErrorMessage.style.display = "block";
+  }
+};
 
 //validation for cvv
 
-function handleValidationForCvv() {
+const handleValidationForCvv = () => {
   let form = document.getElementById("form");
   let cvv = form.cvv.value;
   var key = event.key;
   var numbers = "0123456789";
-  if (event.keyCode !== 8) {
+  if (
+    event.keyCode !== 8 &&
+    event.keyCode !== 37 &&
+    event.keyCode !== 39 &&
+    event.keyCode !== 46
+  ) {
     if (!numbers.includes(key)) {
       event.preventDefault();
     }
   }
-  if (event.keyCode !== 8) {
+  if (
+    event.keyCode !== 8 &&
+    event.keyCode !== 37 &&
+    event.keyCode !== 39 &&
+    event.keyCode !== 46
+  ) {
     if (cvv.length === 3) {
       event.preventDefault();
     }
   }
+};
 
-}
+const hanleCheckPayNowbuttonEnable = () => {
+  let form = document.getElementById("form");
+  let cardNumber = form.cardNumber.value;
+  let cardHolderName = form.cardHolderName.value;
+  let cvv = form.cvv.value;
+  if (
+    cardNumber.length == 16 &&
+    cardHolderName.length != 0 &&
+    cvv.length == 3
+  ) {
+    payNowButtonFlag = true;
+  } else {
+    payNowButtonFlag = false;
+  }
+
+  if (payNowButtonFlag) {
+    let submitButton = document.getElementById("submitButton");
+    submitButton.style.backgroundColor = "purple";
+    submitButton.style.opacity = "1";
+    submitButton.disabled = false;
+  } else {
+    let submitButton = document.getElementById("submitButton");
+    submitButton.style.backgroundColor = "purple";
+    submitButton.style.opacity = "0.5";
+    submitButton.disabled = true;
+  }
+};
