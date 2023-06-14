dc83f3d7c838aecbdfe9ba145f8a7cfba6dd46b8
Pallavee980kri <pallavee.kumari@razorpay.com>
2023-06-14 23:52:06 +0530

    implemmenting_validation_for_all

--- Backend/main.go
+++ Backend/main.go
@@ -3,17 +3,12 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-
-	// "time"
-	"math/rand"
-
-	// "encoding/json"
 	"fmt"
 	"log"
-
-	// "math/rand"
+	"math/rand"
 	"net/http"
-	// "time"
+	// "regexp"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -27,7 +22,6 @@ type Card struct {
 	Expiry_year     int    `json:"expiry_year"`
 	OTP   int    `json:"OTP"`
 }
-
 var db *sql.DB
 func connect()  error {
 	var err error
@@ -41,7 +35,6 @@ func connect()  error {
 		return  fmt.Errorf("failed to ping database: %v", err)
 	}
 	log.Println("Database connected successfully!")
-
     return  nil
 }
 func main() {
@@ -54,8 +47,7 @@ func main() {
 	defer db.Close()
     // Initialize the router
 	router := mux.NewRouter()
-
-	router.HandleFunc("/process_payment", processPaymentHandler).Methods("POST")
+    router.HandleFunc("/process_payment", processPaymentHandler).Methods("POST")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 //access card data from the frontend
@@ -67,29 +59,53 @@ func processPaymentHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Failed to parse JSON payload", http.StatusBadRequest)
 		return
 	}
-
-	log.Printf("Received card data: %+v\n", card)
-
-	if card.Card_number == "" {
+    log.Printf("Received card data: %+v\n", card)
+    if card.Card_number == "" {
 		http.Error(w, "Card holder name is required", http.StatusBadRequest)
 		return
 	}
-	if card.Card_number == "" || len(card.Card_number) != 16 {
-        http.Error(w,"Card number is required and must be 16 digits Please enter valid card number",http.StatusBadRequest)
-		return
-	}
+	//validation for card number 
+		var errorMessages []string//creating a slice
+if card.Card_number == "" {
+	errorMessages = append(errorMessages, "Card number is required.")
+}
 
-	if(card.CVV=="" || len(card.CVV)!=3){
+if len(card.Card_number) != 16 {
+	errorMessages = append(errorMessages, "Card number must be 16 digits.")
+}
+
+if strings.Contains(card.Card_number, "-") {
+	errorMessages = append(errorMessages, "Card number cannot contain '-' character.")
+}
+
+if strings.Contains(card.Card_number, "+") {
+	errorMessages = append(errorMessages, "Card number cannot contain '+' character.")
+}
+
+if strings.Contains(card.Card_number, "e") {
+	errorMessages = append(errorMessages, "Card number cannot contain 'e' character.")
+}
+
+if strings.Contains(card.Card_number, " ") {
+	errorMessages = append(errorMessages, "Card number cannot contain whitespace.")
+}
+
+if len(errorMessages) > 0 {
+	errorString := strings.Join(errorMessages, " ")
+	http.Error(w, errorString, http.StatusBadRequest)
+	return
+}
+
+
+    if(card.CVV=="" || len(card.CVV)!=3){
 		http.Error(w,"CVV is required Please enter valid 3 digits cvv number",http.StatusBadRequest)
 		return
 	}
-
-	if(card.Expiry_month==0||card.Expiry_year==0){
+    if(card.Expiry_month==0||card.Expiry_year==0){
 		http.Error(w,"Expiry month and year are required",http.StatusBadRequest)
 		return
 	}
-
-	// Check if the card data exists in the database
+    // Check if the card data exists in the database
 	query := "SELECT * FROM card_information WHERE card_number = ? AND cardholder_name = ?"
 	row := db.QueryRow(query, card.Card_number, card.Cardholder_name)
     var storedCard Card
@@ -104,13 +120,10 @@ func processPaymentHandler(w http.ResponseWriter, r *http.Request) {
     &storedCard.Expiry_year,
     &otpValue,
 )
-
-	
-	if err == sql.ErrNoRows {
+    if err == sql.ErrNoRows {
 		// Card data not found in the database
 		log.Println("Error in card data founding:", err)
-
-		http.Error(w, "Card data not found", http.StatusNotFound)
+        http.Error(w, "Card data not found", http.StatusNotFound)
 		return
 	} else if err != nil {
 		log.Println("Error querying the database:", err)
@@ -118,49 +131,35 @@ func processPaymentHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	log.Println("Card ID:", card.ID)
-
-	// log.Println("storedcard",storedCard)
-	// log.Println("Stored Card Number:", storedCard.Card_number)
-	// log.Println("Stored Cardholder Name:", storedCard.Cardholder_name)
-	// log.Println("Frontend Card Number:", card.Card_number)
-	// log.Println("Frontend Cardholder Name:", card.Cardholder_name)
-	// log.Println("Frontend CVV:", card.CVV)
-	// log.Println("Stored CVV:", storedCard.CVV)
-	// log.Println("Frontend Expiry Month:", card.Expiry_month)
-	// log.Println("Stored Expiry Month:", storedCard.Expiry_month)
-	// log.Println("Frontend Expiry Year:", card.Expiry_year)
-	// log.Println("Stored Expiry Year:", storedCard.Expiry_year)
-
-	// Compare the stored card data with the frontend data
+    // Compare the stored card data with the frontend data
 	if card.CVV != storedCard.CVV || card.Expiry_month != storedCard.Expiry_month || card.Expiry_year != storedCard.Expiry_year {
 		http.Error(w, "Card data does not match", http.StatusBadRequest)
 		return
 	}
-
-	// Generate OTP
-otp := generateOTP()
-
-// Update the OTP in the database
-updateQuery := "UPDATE card_information SET OTP = ? WHERE id = ?"
-_, err = db.Exec(updateQuery, otp, storedCard.ID)
-if err != nil {
+    // Generate OTP
+    otp := generateOTP()
+    // Update the OTP in the database
+    updateQuery := "UPDATE card_information SET OTP = ? WHERE id = ?"
+    _, err = db.Exec(updateQuery, otp, card.ID)
+    if err != nil {
     log.Println("Error updating OTP in the database:", err)
     http.Error(w, "Failed to update OTP in the database", http.StatusInternalServerError)
     return
 }
-
-log.Println("OTP",otp)
+    log.Println("OTP:",otp)
 	// Send a response back to the frontend
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OTP added successfully"))
-	w.Write([]byte("Payment processed successfully"))
+	w.Write([]byte("OTP added successfully AND Payment processed successfully"))
 }
 
 // Function to generate a random OTP
-
 func generateOTP() int {
-
-	otp := rand.Intn(900000) + 100000
+    otp := rand.Intn(900000) + 100000
 	return otp
 
 }
+//validation for card number of -+e and space character
+// func containsInvalidCharsInCardNumber(cardNumber string) bool {
+	// regex := regexp.MustCompile(`[-+e\s]`)
+	// return regex.MatchString(cardNumber)
+// }
